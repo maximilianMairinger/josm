4f72d22571c0c3ebd002c1181a7f8410ca24e74c
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2022-03-17 18:11:35 +0100

    Array add and forEach working semi with types

--- repl/src/repl.ts
+++ repl/src/repl.ts
@@ -1,230 +1,29 @@
-import { Data as DATA, DataBase as DATABASE, DataBaseSubscription, DataCollection, DataSubscription, setDataDerivativeIndex } from "../../app/src/josm"
-import constructIndex from "key-index"
-import constructAttatchToPrototype from "attatch-to-prototype"
-import clone from "fast-copy"
-import { constructObjectIndex } from "key-index"
+import { Data, DataBase } from "./replLib"
 
 
 
-const { Data, setDataBaseDerivativeIndex, parseDataBase } = setDataDerivativeIndex(
-  class NumberData<T extends number> extends DATA<T> {
-    inc(by: number = 1) {
-      this.set((this.get() as any + by))
-      return this
-    }
-  }
-)
 
-const ExDataBase = parseDataBase(DATABASE)
-const eee = new ExDataBase({e: {a: 2}, a: 3})
-
-
-const DataBase = setDataBaseDerivativeIndex(
-  class ArrayList<T extends string> extends ExDataBase<T[]> {
-    forEach(addedCb: (added: DATA<T>, i: number) => ((() => void) | DataSubscription<[...any[]]>)) {
-      const destroyMap = new Map<DATA, Function>()
-
-      this((full, added, removed) => {
-        for (const key in added) {
-          const numKey = +key
-          if (isNaN(numKey)) continue
-          const ret = addedCb((this as any)[key], numKey)
-          if (ret instanceof DataSubscription) destroyMap.set((this as any)[key], ret.deactivate.bind(ret))
-          else if (ret instanceof Function) destroyMap.set((this as any)[key], ret)
-        }
-
-        for (const key in removed) {
-          const numKey = +key
-          if (isNaN(numKey)) continue
-          if (destroyMap.has((removed as any)[key])) {
-            destroyMap.get((removed as any)[key])()
-            destroyMap.delete((removed as any)[key])
-          }
-        }
-
-      }, false, false)
-
-
-      for (const key in this) {
-        const numKey = +key
-        if (isNaN(numKey)) continue
-        const ret = addedCb((this as any)[key], numKey)
-        if (ret instanceof DataSubscription) destroyMap.set((this as any)[key], ret.deactivate.bind(ret))
-        else if (ret instanceof Function) destroyMap.set((this as any)[key], ret)
-      }
-
-    }
-  },
-  class WutOBase extends ExDataBase<{wut: number}> {
-    incWut(by: number = 1) {
-      this.wut.inc(by)
-      return this
-    }
-  }
-)
-
-
-const db = new DataBase({e: ["elem1", "elem2", "elem3"]})
-db.e.forEach((added) => {
-  console.log("added", added.get())
-  const sub = added.get((val) => {
-    console.log("changed", val)
-  }, false)
-
-  return () => {
-    console.log("removed", added)
-    sub.deactivate()
-  }
+const db = new DataBase({
+  helloKey: 2,
+  arrayKey: [1, 2, 3],
 })
-
-console.log("hehe2")
-
-
-db.e([1,2], true)
-db.e({"1": "change 2"})
-const data1 = db.e[1]
-data1.set("change 22")
-db.e({"1": undefined})
-data1.set("change wihasbhjasbhj")
 
 // @ts-ignore
 window.db = db
 
-
-// console.log(new Data(2).inc(2).get())
-
-
-
+const func = db((full) => {
+  console.log(full)
+})
 
 
-
-
+db.helloKey.inc(6)
+db.arrayKey.add(4)
 
 
 
+func.deactivate()
 
-
-
-
-// const db = new DataBase({
-//   helloKey: "HELLO"
-// }, {
-//   helloKey: "helloDefault"
-// })
-
-// db((e, q) => {
-//   console.log(e, q)
-// })
-
-// // db.helloKey.set(undefined)
-// db(undefined, true)
-
-
-// console.log("done")
-
-
-
-
-
-
-
-
-// const data = new DataBase({lol: "aa"})
-// const sub1 = data((e) => {
-//   console.log("sub1", e)o
-// })
-
-// const sub2 = data((e) => {
-//   console.log("sub2", e)
-// })
-// const sub3 = data((e) => {
-//   console.log("sub3", e)
-// })
-
-// debugger
-// sub2.deactivate()
-// sub1.deactivate()
-// sub2.activate()
-
-
-// data({lol: "bb"})
-
-// sub.setToDataBase({lol: "cc"})
-
-
-
-
-// const db = new DataBase({})
-// const sub = db((e, diff) => {
-//   console.log("diff")
-//   console.log(diff)
-// }, true) as any as DataBaseSubscription<[{a?: string, b?: string, c?: string}]>
-
-// db({
-//   a: "a",
-//   b: "b"
-// })
-
-// db({
-//   c: "c"
-// })
-
-// console.log("deactiv")
-// sub.deactivate()
-
-
-// db({
-//   a: "aa",
-//   c: "cc"
-// })
-
-// db({
-//   c: "ccc"
-// })
-
-// console.log("active")
-// sub.activate()
-
-
-
-
-
-
-
-
-
-// let data1 = new Data("1")
-// let data3: Data<string>
-// let d: Data<number>
-// let first = true
-// data1.get(() => {
-//   console.log("d1 call")
-//   if (first) {
-//     first = false
-    
-//     data3 = new Data("3")
-//     d = data3.tunnel(d => +d)
-//   }
-  
-//   d.get((e) => console.log(e))
-//   data3.get((e) => console.log(e))
-// })
-
-
-// data1.set("11")
-// data3.set("33")
-// data3.set("44")
-
-
-// let dataA = new Data("a")
-// let dataB = new Data("b")
-
-// dataA.get((a) => {
-//   console.log(a)
-//   dataB.get((b) => {
-//     console.log(b)
-//   })
-// })
-
-// dataA.set("aa")
-// dataB.set("bb")
+db.helloKey.inc(6)
+console.log("done")
+func.activate(false)
+console.log("done 4 real")
