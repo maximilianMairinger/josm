558e2fbc22fe207774fdeb722fd8e9046ab564e4
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-02-19 21:11:11 +0100

    ReadonlyData type

--- app/src/josm.ts
+++ app/src/josm.ts
@@ -2,3 +2,7 @@ export { setDataDerivativeIndex } from "./derivativeExtension"
 export { Data, DataBaseSubscription, DataSubscription } from "./dataDerivable"
 export { DataCollection } from "./dataCollection"
 export { DataBase, parsingId as dataBaseParsingId, internalDataBaseBridge } from "./dataBaseDerivable"
+
+
+import { Data } from "./dataDerivable"
+export type ReadonlyData<T> = Omit<Data<T>, "set">
